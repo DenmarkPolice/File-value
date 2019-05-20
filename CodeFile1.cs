a2aefbe4a19da2379248e25cd93fba27c40bb5c9
XALPE <XALPE@C812>
2019-05-20 15:23:36 +0200

    Add project files.

---
+++ CodeFile1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace CodeFile1
+{
+
+    class RunArray
+    {
+
+        static void Main()
+        {
+            var WriteConsole = new WriteConsole();
+            WriteConsole.Array();
+        }
+    }
+
+    class WriteConsole
+    {
+
+        String[] arr = File.ReadAllLines("test.txt");
+        public void Array()
+        {
+            arr[0] = ".";
+            int counter = 0;
+
+            while (counter < arr.Length)
+            {
+                if(arr[counter] != ".")
+                {
+                    Console.WriteLine(arr[counter]);
+                }
+                counter += 1;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
